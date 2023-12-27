88ff23752bfced616cd5ad1c060be15464983916
askar <maeaxvx@gmail.com>
2023-12-27 14:39:50 +0200

    adding java script multiblication code

--- library_proj/project/lms_app/forms.py
+++ library_proj/project/lms_app/forms.py
@@ -24,6 +24,7 @@ class BookForm(forms.ModelForm):
             'price',
             'retal_price_day',
             'retal_period',
+            'total_rental',
             'status',
             'category',
         ]                    # you can get all the fields by value :      '__all__'
@@ -35,8 +36,9 @@ class BookForm(forms.ModelForm):
             'photo_author' :forms.FileInput(attrs={'class': 'form-control'}),   
             'pages' :forms.NumberInput(attrs={'class': 'form-control'}),
             'price' :forms.NumberInput(attrs={'class': 'form-control'}),
-            'retal_price_day' :forms.NumberInput(attrs={'class': 'form-control'}),
-            'retal_period' :forms.NumberInput(attrs={'class': 'form-control'}),
+            'retal_price_day' :forms.NumberInput(attrs={'class': 'form-control', 'id':'rentalprice'}),
+            'retal_period' :forms.NumberInput(attrs={'class': 'form-control', 'id':'rentaldays'}),
+            'total_rental' :forms.NumberInput(attrs={'class': 'form-control', 'id':'totalrental'}),
             'status' :forms.Select(attrs={'class': 'form-control'}),
             'category' :forms.Select(attrs={'class': 'form-control'}),
 
